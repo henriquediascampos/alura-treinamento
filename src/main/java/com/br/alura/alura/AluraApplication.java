68a1dd3eec148ba41a07024a8b6bbac2a5a53f6a
Henrique Dias Campos <henriquediascampos01@gmail.com>
2018-08-05 09:19:12 -0300

    init

---
+++ src/main/java/com/br/alura/alura/AluraApplication.java
@@ -0,0 +1,12 @@
+package com.br.alura.alura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AluraApplication.class, args);
+	}
+}
---
+++ src/test/java/com/br/alura/alura/AluraApplicationTests.java
@@ -0,0 +1,16 @@
+package com.br.alura.alura;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AluraApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
